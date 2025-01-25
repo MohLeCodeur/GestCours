c819a772dfcce12e5c7ad53a1b7a7d67a1c55d26
mohlecodeur <mohlecodeur@gmail.com>
2025-01-25 19:47:58 +0000

    m

--- Gestionlistecourse/settings.py
+++ Gestionlistecourse/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0#(3x7i890e%=l4nd($g-4p(+ev*k=9$pcpt5^uzeg7^(@l_y-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Gestionlistecourse.wsgi:application
